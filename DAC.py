5a111653a67c22f58e9026e84c01431d6014cc3c
Mitrek <andrelucasleandro@gmail.com>
2021-09-05 13:00:39 -0300

    Update DAC.py

--- DAC.py
+++ DAC.py
@@ -5,10 +5,8 @@
 import pyautogui
 import keyboard
 import datetime
-from pyautogui import click
-from pyautogui import write
-from pyautogui import press
 from time import sleep
+
 
 #Functions
 
@@ -18,24 +16,47 @@ def list_formatter(list, to_remove = "\n"):
         formatted_line = line.rstrip("\n")
         r_list.append(formatted_line)
     return r_list
-def billing_message(billed):
-    if "RN" in billed:
-        bill_size = 2
-    if "MG" in billed:
-        bill_size = 1
-    if "ES" in billed:
-        bill_size = 0
-    billed_name = billed
-    billing_m = "Ola " + billed_name + ", faltam "
+    
+    
+def days_countdown():
     now = datetime.datetime.now()
     end_time = datetime.datetime(2024, 8, 23, 8, 0, 0)
     difference = str(end_time - now).split()[0]
-    billing_m += difference + " dias para nossa grande formatura. Por esse motivo a sua COMFASP conta com sua ajuda com a contribuicao mensal, segue o link para PIX:\n"
+    return difference
+    
+def billing_message(billed):
+    if "RN" in billed:
+        bill_size = 2
+    elif "MG" in billed:
+        bill_size = 1
+    elif "ES" in billed:
+        bill_size = 0
+        
+    billed_name = billed #Fineza ignorar essa linha por enquanto
+    disclaimer = "Disclaimer: Se voce nao se chama " + billed_name + " ou tambem nao sabe o que e a COMFASP, fineza ignorar a mensagem e de maneira nenhuma fazer qualquer tipo de transacao bancaria \n"
+    billing_m = disclaimer
+    billing_m += "Ola " + billed_name + ", faltam "
+    billing_m += days_countdown()
+    billing_m += " dias para nossa grande formatura. Por esse motivo a COMFASP conta com sua ajuda com a contribuicao mensal, segue o link para PIX:\n"
     billing_m += billing_link
-    disclaimer = "\n Disclaimer: Se voce nao se chama " + billed_name + " ou tambem nao sabe o que e a COMFASP, fineza ignorar a mensagem e de maneira nenhuma fazer qualquer tipo de transacao bancaria"
-    billing_m += disclaimer
+    
     return billing_m
     
+    
+#Decorators
+
+def write(x):
+    pyautogui.write(x)
+    sleep(delay)
+
+def press(x):
+    pyautogui.press(x)
+    sleep(delay)
+    
+def click(*args):
+    pyautogui.click(args[0],args[1])
+    sleep(delay)
+
     
 #Globals
 
@@ -52,11 +73,8 @@ for x in range(len(bill_list)):
     keyboard.wait("insert")
     click(280,280)
     write(billed)
-    sleep(delay)
     click(300,380)
-    sleep(delay)
     click(790,810)
-    sleep(delay)
     write(billing_message(billed))
     press("enter")
     
